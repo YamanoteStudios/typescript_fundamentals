8fdbe7d24f3b4ef0595aee995ffed44840134c89
Chris Samuel <ksamuel.chris@gmail.com>
2018-01-18 12:16:14 -0500

    removed console logs

--- index.ts
+++ index.ts
@@ -2,13 +2,19 @@ for(let index = 0; index < 101; index++) {
 var isFizz = index % 3 === 0;
 var isBuzz = index % 5 === 0;
 var isFizzBuzz = (isFizz && isBuzz);
-console.log(index);
+
+let result;
+// console.log(index);
 if (isFizzBuzz) {
     // print the desired condition
-    console.log('Fizz');
-} else if (isBuzz) {
-    console.log('Buzz');
+    // console.log('Fizz');
+    result = 'isFizzBuzz';
+} else if (isFizz) {
+    // console.log('Buzz');
+    result = 'isBuzz';
  } else if (isBuzz)  {
-        console.log('FizzBuzz');
+        // console.log('FizzBuzz');
+        result = 'Fizz';
+
  }
 }
