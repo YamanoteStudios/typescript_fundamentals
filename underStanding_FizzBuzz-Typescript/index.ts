caa2f111c02d37ae5c90033d6db679481b3aa46b
Chris Samuel <ksamuel.chris@gmail.com>
2018-01-18 12:22:12 -0500

    pushed to example folder

--- underStanding_FizzBuzz-Typescript/index.ts
+++ underStanding_FizzBuzz-Typescript/index.ts
@@ -1,14 +1,15 @@
 for(let index = 0; index < 101; index++) {
-var isFizz = index % 3 === 0;
-var isBuzz = index % 5 === 0;
-var isFizzBuzz = (isFizz && isBuzz);
-console.log(index);
-if (isFizzBuzz) {
-    // print the desired condition
-    console.log('Fizz');
-} else if (isBuzz) {
-    console.log('Buzz');
- } else if (isBuzz)  {
-        console.log('FizzBuzz');
- }
+const isFizz = index % 3 === 0;
+const isBuzz = index % 5 === 0;
+const isFizzBuzz = (isFizz && isBuzz);
+
+let result;
+
+isFizz && isBuzz ? 'FizzBuzz'
+      : isFizz
+        ? 'Fizz'
+        : isBuzz
+          ? 'Buzz'
+          : index;
+ console.log(result);
 }
