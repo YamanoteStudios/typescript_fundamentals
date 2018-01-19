37c3c35b3d096903ba725e6eae4fa24f943f12ee
Chris Samuel <ksamuel.chris@gmail.com>
2018-01-19 17:03:32 -0500

    explicitly returning the recursive inner calls

--- binarySearch_typescript.ts
+++ binarySearch_typescript.ts
@@ -20,15 +20,16 @@ function binarySearch(arr, i) {
     var mid = Math.floor(arr.length / 2);
     console.log(arr[mid], i);
 
+    // ...
     if (arr[mid] === i) {
         console.log('match', arr[mid], i);
         return arr[mid];
     } else if (arr[mid] < i && arr.length > 1) {
         console.log('mid lower', arr[mid], i);
-        binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
+        return binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
     } else if (arr[mid] > i && arr.length > 1) {
         console.log('mid higher', arr[mid], i);
-        binarySearch(arr.splice(0, mid), i);
+        return binarySearch(arr.splice(0, mid), i);
     } else {
         console.log('not here', i);
         return -1;
