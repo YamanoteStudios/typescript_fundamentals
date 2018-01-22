59f12fa786689702c1598bdc00e4e62dae8fdf8e
Chris Samuel <ksamuel.chris@gmail.com>
2018-01-22 15:40:20 -0500

    added a insertionSort example

---
+++ sorting/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Chris Samuel Jr
+ * chris@kayode.me
+ * 
+ * github: Alayode
+ */
+
+
+ /**
+ * Sorts an array using insertion sort
+ */
+  function insertionSort(){}
+  
+  const unsortedArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+  
+  console.log(insertionSort(unsortedArray))
+  
+
+  
