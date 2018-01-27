218c810301ade95fdad37ec17ba9c1ba76c74282
Chris Samuel <ksamuel.chris@gmail.com>
2018-01-27 13:56:03 -0500

    sorted array at a time cost for the recursive work in Merge Sort

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 */
 function mergeSort(array) {
     if (array.length <= 1) {
-        console.log(array);
+        // console.log(array);
         return array;
     }
     //we first find the middle index by dividing the array length by two.
@@ -25,5 +25,35 @@ function mergeSort(array) {
     return merge(mergeSort(left), mergeSort(right));
 }
 exports.mergeSort = mergeSort;
-var array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // a single element in the array is already sorted.
-mergeSort(array); // returns 5
+/** merge (conquer) step of MergeSort */
+function merge(left, right) {
+    var array = [];
+    var lIndex = 0;
+    var rIndex = 0;
+    while (lIndex + rIndex < left.length + right.length) {
+        // console.log(left[lIndex]);
+        // console.log(right[rIndex]);
+        var lItem = left[lIndex];
+        var rItem = right[rIndex];
+        if (lItem == null) {
+            array.push(rItem);
+            rIndex++;
+        }
+        else if (rItem == null) {
+            array.push(lItem);
+            lIndex++;
+        }
+        else if (lItem < rItem) {
+            array.push(lItem);
+            lIndex++;
+        }
+        else {
+            array.push(rItem);
+            rIndex++;
+        }
+    }
+    return array;
+}
+var unsortedArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // a single element in the array is already sorted.
+mergeSort(unsortedArray); // returns 5
+console.log(mergeSort(unsortedArray));
--- index.ts
+++ index.ts
@@ -16,10 +16,10 @@
 
 export function mergeSort(array: number[]): number[] {
   if(array.length <= 1) {
-    console.log(array);
+    // console.log(array);
     return array;
   }
-  
+
 //we first find the middle index by dividing the array length by two.
 const middle = Math.floor(array.length / 2 );
 // console.log(middle);
@@ -32,9 +32,37 @@ return merge(mergeSort(left), mergeSort(right))
 
 }
 
+/** merge (conquer) step of MergeSort */
+
+function merge(left: number[], right: number[]): number[] {
+const array: number[] = [];
+let lIndex = 0;
+let rIndex = 0;
+while(lIndex + rIndex <left.length + right.length) {
+  // console.log(left[lIndex]);
+  // console.log(right[rIndex]);
+   const lItem = left[lIndex];
+   const rItem = right[rIndex];
+    if(lItem == null) {
+      array.push(rItem); rIndex++;
+    }
+    else if (rItem == null) {
+      array.push(lItem); lIndex++;
+    }
+    else if (lItem < rItem) {
+      array.push(lItem); lIndex++;
+    }
+    else {
+      array.push(rItem); rIndex++;
+    }
+  }
+ return array;
+}
 
 
+const unsortedArray = [10,9,8,7,6,5,4,3,2,1,0]; // a single element in the array is already sorted.
 
-const array = [10,9,8,7,6,5,4,3,2,1,0]; // a single element in the array is already sorted.
+mergeSort(unsortedArray); // returns 5
 
-mergeSort(array); // returns 5
+
+console.log(mergeSort(unsortedArray))
